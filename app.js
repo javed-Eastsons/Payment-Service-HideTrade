3f5f17f26e8bc184e2463150c59df25db1d3abd8
Arin Modi <arinmodi2306@gmail.com>
2023-01-19 19:52:42 +0530

    Subscription APIs

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const stripe = require('stripe')('sk_test_51MMu5kLt3bt57eoNf5hXuaIKsIjrIqGHkeEUNagFlQDm4p1LcxXOc9hkbDsjQVlyitl0UjzxwrYzAiz846KBeURE00tBwSYqxc');
 
+const axios = require("axios");
+
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -22,46 +24,209 @@ app.get("/", (_req, res) => {
 	res.status(200).json({ message: "Hello There!! You are at Backend" });
 });
 
-app.post("/payment", async (req, res) => {
-	
-	// validating amount
+app.post('/webhooks', express.raw({type: 'application/json'}), async (req, res) => {
+    let event = req.body;
 
-    let amount = (req.body.amount+"").trim();
+    console.log(event.type);
 
-    // amount empty or undefined
-    if(req.body.amount === undefined || amount === "") {
-        return res.status(400).send("Invalid data");
-    } 
+    switch (event.type) {
+        case "invoice.paid":
+            await subscription_created(req);
+            break;
+        case "checkout.session.completed":
+            await updatePaymentMethod(req);
+        default:
+            res.status(400).end();
+    }
+});
 
-    // amount must be number
-    let isNum = /^\d{1,}\.?\d{0,2}$/.test(amount);
-    if(!isNum) return res.status(400).send("Invalid data");
+app.post("/create-subscription", async (req, res) => {
+    console.log(req.body);
 
-    amount = parseInt(amount);
+    const customers = await stripe.customers.list({
+        email : req.body.email
+    });
 
-    // amount must be greater than zero
-    if(amount <= 0) return res.status(400).send("Invalid data");;
+    let cid = "";
 
-    var paymentIntent;
-    try{
-        paymentIntent = await stripe.paymentIntents.create({
-            amount: amount,
-            currency: 'usd',
-            payment_method_types: ['card'],
+    if (customers.data.length == 0) {
+        const customer = await stripe.customers.create({
+            email: req.body.email,
+            name: req.body.name
         });
-    }catch(e) {
-        console.log(e)
-        res.status(400).send("Error");
-    } 
+        cid = customer.id;
+    } else {
+        console.log("Exists...");
+        cid = customers.data[0].id;
+    }
+
+    const priceId = "price_1MQn2zLt3bt57eoN62NEtelM";
+
+    try {
+        const subscription = await stripe.subscriptions.create({
+          customer: cid,
+          items: [{
+            price: priceId,
+          }],
+          payment_behavior: 'default_incomplete',
+          payment_settings: { save_default_payment_method: 'on_subscription' },
+          expand: ['latest_invoice.payment_intent'],
+        });
+    
+        res.send({
+          subscriptionId: subscription.id,
+          clientSecret: subscription.latest_invoice.payment_intent.client_secret,
+          customerId : cid
+        });
+    } catch (error) {
+        return res.status(400).send({ error: { message: error.message } });
+    }
+});
+
+app.post("/retrive-subsciption", async (req, res) => {
+    console.log(req.body);
+    if (req.body.subId === undefined) {
+        return res.status(400).send("Invalid Id");
+    }
+
+    const subsciption = await stripe.subscriptions.retrieve(
+        req.body.subId
+    );
 
     res.status(200).send({
-		paymentIntent : paymentIntent,
-		publishableKey : "pk_test_51MMu5kLt3bt57eoN31c7NdtotLS2VsUgzf3TPC3fzhCjAu3Wqa3mOAV1SpSjWPInyFdsAnf5wf2kIutILMpPORDt00tdY821ah"
-	})
+        payId : subsciption.default_payment_method,
+        cancel : subsciption.cancel_at_period_end
+    })
 });
+
+app.post("/get-payment-method-details", async (req, res) => {
+    if (req.body.payId === undefined) {
+        return res.status(400).send("Invalid Id");
+    }
+
+    const paymentMethod = await stripe.paymentMethods.retrieve(
+        req.body.payId
+    );
+
+    return res.status(200).send({
+        data : paymentMethod
+    });
+});
+
+app.post("/cancel-subscription-update", async (req, res) => {
+    if (req.body.subId === undefined || req.body.status === undefined) {
+        return res.status(400).send("Invalid Id");
+    }
+
+    const paymentMethod = await stripe.subscriptions.update(
+        req.body.subId, 
+        {cancel_at_period_end: req.body.status}
+    );
+
+    return res.status(200).send({
+        data : paymentMethod
+    });
+});
+
+app.post("/checkout-session", async (req, res) => {
+    console.log(req.body);
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'setup',
+        customer: req.body.cusId,
+        setup_intent_data: {
+          metadata: {
+            customer_id: req.body.cusId,
+            subscription_id: req.body.subId,
+          },
+        },
+        success_url: 'https://example.com/success?sc_checkout=success',
+        cancel_url: 'https://example.com/cancel?sc_checkout=cancel',
+    });
+
+    res.status(200).send({
+        session : session
+    });
+});
+
+const subscription_created = async (req) => {
+    const cid = req.body.data.object.customer;
+
+    const responseOfCid = await stripe.customers.retrieve(cid);
+    const userId = responseOfCid.name;
+
+    const subscription = await stripe.subscriptions.list({
+        limit : 1,
+        customer : cid
+    });
+
+    const subId = subscription.data[0].id;
+    const endTimeStamp = subscription.data[0].current_period_end;
+    const status = subscription.data[0].status;
+
+    console.log("Sub Id : " + subId);
+    console.log("End Time Stamp : " + endTimeStamp);
+
+    console.log(status)
+
+    // update into the api
+
+    if (status === "active") {
+        console.log("updating into database");
+
+        const params = new URLSearchParams();
+
+        params.append('user_id', userId);
+        params.append('subscription_id', subId);
+        params.append('customer_id', cid);
+        params.append('timestamp', endTimeStamp);
+    
+        let url = "https://refuel.site/projects/hidetrade/APIs/UpdateProfile/UpdateProfileWithSubscription.php";
+    
+        axios.post(url, params).then((result) => {
+            console.log(result);
+            console.log(result.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+};
+
+const updatePaymentMethod = async (req) => {
+    const setupIntentId = req.body.data.object.setup_intent;
+    console.log(setupIntentId);
+
+    const setupIntent = await stripe.setupIntents.retrieve(setupIntentId);
+
+    const cusId = setupIntent.customer;
+    const subId = setupIntent.metadata.subscription_id;
+    const payId = setupIntent.payment_method;
+
+    console.log(cusId);
+    console.log(subId);
+    console.log(payId);
+
+    console.log("upadting the customer default method : ");
+
+    const customer = await stripe.customers.update(
+        cusId,
+        {invoice_settings: {default_payment_method: payId}}
+    );
+
+    console.log(customer);
+
+    console.log("updating the subscription");
+
+    const subscription = await stripe.subscriptions.update(subId, {
+        default_payment_method: payId,
+    });
+
+    console.log(subscription);
+}
 
 // handle the error safely
 process.on("uncaughtException", (err) => {
+    console.log(req);
 	console.log(err);
 });
 
