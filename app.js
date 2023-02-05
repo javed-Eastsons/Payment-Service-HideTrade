73b48034f4275b3fb95482501c5a2227881399c8
Arin Modi <arinmodi2306@gmail.com>
2023-02-05 10:54:10 +0530

    Minor Fix

--- app.js
+++ app.js
@@ -70,13 +70,6 @@ app.post("/create-subscription", async (req, res) => {
         cid = customers.data[0].id;
     }
 
-    const ephemeralKey = await stripe.ephemeralKeys.create(
-        {customer: cid},
-        {apiVersion: '2022-11-15'}
-    );
-
-    console.log(ephemeralKey);
-
     const priceId = "price_1MUTgMLt3bt57eoNzHjeTyOF";
 
     try {
@@ -95,8 +88,7 @@ app.post("/create-subscription", async (req, res) => {
         res.send({
           subscriptionId: subscription.id,
           clientSecret: subscription.latest_invoice.payment_intent.client_secret,
-          customerId : cid,
-          key : ephemeralKey.secret
+          customerId : cid
         });
     } catch (error) {
         return res.status(400).send({ error: { message: error.message } });
