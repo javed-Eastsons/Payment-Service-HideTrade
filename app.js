f340fca2afa730ac6045a5e742bdafdf7fc6f783
unknown <javed@eastsons.com>
2023-09-14 18:50:18 +0530

    live key

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 
-// const stripe = require('stripe')('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');
-const stripe = require('stripe')('sk_test_51MMu5kLt3bt57eoNf5hXuaIKsIjrIqGHkeEUNagFlQDm4p1LcxXOc9hkbDsjQVlyitl0UjzxwrYzAiz846KBeURE00tBwSYqxc');
+const stripe = require('stripe')('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');
+
 const axios = require("axios");
 
 app.use(function(req, res, next) {
