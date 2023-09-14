9e2147f6f49e014758e3a8a89d71ff06e627176b
javed-Eastsons <135799888+javed-Eastsons@users.noreply.github.com>
2023-09-15 01:52:31 +0530

    add express json app.js

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(function(req, res, next) {
 app.use(cors({origin: true, credentials: true}));
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 // Home route
 app.get("/", (_req, res) => {
