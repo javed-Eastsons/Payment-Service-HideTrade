5749762268489e1c563c25565c2e55d026f39e7e
javed-Eastsons <135799888+javed-Eastsons@users.noreply.github.com>
2023-09-15 01:31:00 +0530

    again add webhooks app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get("/", (_req, res) => {
 });
 app.use(express.urlencoded({ extended: true })); // Middleware to parse form data
 
-app.post('/webhooks', async (req, res) => {
+app.post('/webhooks', express.raw({ type: "application/json" }), async (req, res) => {
     let event = req.body;
 
     console.log(event.type);
